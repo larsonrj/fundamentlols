269ae64763033683bda9b493cb7101b9f0b29a3c
Ryan <rylarson17@gmail.com>
2024-01-26 15:57:07 -0700

    Updated server to add graphql

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,7 +5,7 @@ const key = process.env.REACT_APP_API_KEY;
 const search = (query) =>
   axios
     .get(
-      `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${query}?api_key=${key}`
+      `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${query}/NA1?api_key=${key}`
     )
     .then((res) => {
       console.log(res.data);
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
 // Wrap Mongoose around local connection to MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/fundamentlolsDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/fundamentlolsDB",
+  // Options for newer version of mongoose. These will remove warnings that would otherwise print out
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 // Export connection
 module.exports = mongoose.connection;
