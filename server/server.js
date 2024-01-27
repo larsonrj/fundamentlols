621d24301980312ad3fea4d8cf06fab84285c44a
larsonrj <rylarson17@gmail.com>
2024-01-26 21:40:54 -0700

    Added folders and files for graphql schema

--- server/server.js
+++ server/server.js
@@ -1,18 +1,15 @@
-// Require express from package.json and require the db connection and routes
-const express = require("express");
+// Require express from package.json
+// Common JS
+// const express = require("express");
+// ES6
+import express from "express";
+
+// Import apollo server class
+const { ApolloServer } = require("apollo-server-express");
+
+// Require GraphQL schema and database (mongoDB)
+const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
-const routes = require("./routes");
 
-// Create express app and listening port
-const PORT = process.env.PORT || 3001;
 const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// app.use(routes);
-
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server for fundamentlols running on port ${PORT}!`);
-  });
-});
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
